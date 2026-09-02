74f3cec98b673b3b29d4dce6eef221de30851362
Aliffatur Rifki <123286233+armpxz@users.noreply.github.com>
2026-09-02 19:05:28 +0700

    Remove copyright from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,19 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Made with Love by Aliffatur Rifki</h3>
+        <Col md="6" className="footer-copywright">
+          <h3>Made with ❤️ by Aliffatur Rifki</h3>
         </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Aliffatur Rifki</h3>
-        </Col>
-        <Col md="4" className="footer-body">
+        <Col md="6" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
                 href="https://github.com/soumyajit4419"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
@@ -35,7 +30,7 @@ function Footer() {
               <a
                 href="https://twitter.com/Soumyajit4419"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiOutlineTwitter />
@@ -45,7 +40,7 @@ function Footer() {
               <a
                 href="https://www.linkedin.com/in/soumyajit4419/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
@@ -55,7 +50,7 @@ function Footer() {
               <a
                 href="https://www.instagram.com/soumyajit4419"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
